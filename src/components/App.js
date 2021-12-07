1d2ab6ca4d7823b1e90feeac80c606188cc6c7b0
Perrine Lardilleux <perrinelardilleux@gmail.com>
2021-12-07 20:38:26 -0300

    dropdown list open and closed

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './Accordion';
 import Search from './Search';
+import Dropdown from './Dropdown';
 
 const items = [
   {
@@ -17,10 +18,31 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The color Red',
+    value: 'red'
+  },
+  {
+    label: 'The color Green',
+    value: 'green'
+  },
+  {
+    label: 'Shade of blue',
+    value: 'blue'
+  },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,64 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('programming');
+  const [results, setResults] = useState([]);
 
-  console.log('I  RUN AT EVERY RENDER')
+  console.log(results);
+  console.log('I  RUN AT EVERY RENDER');
 
   useEffect(() => {
-    console.log('I ONLY RUN ONCE')
-  }, []);
+    console.log('I ONLY RUN ONCE');
+
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    if(term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={results.pageid} className="item">
+        <div className="right floated content">
+          <a
+          className="ui button"
+          href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -21,8 +72,11 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">
+        {renderedResults}
+      </div>
+        }
     </div>
-
   );
 };
 
